8d93c7307a3a663bb070c4a9234cfe8821edfb53
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-15 16:31:14 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './Banner.css'
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className='banner-container'>
+            <h1>Best foot waiting for you.</h1>
+            <div className="search">
+                <input type="text" placeholder='Search food items' />
+                <button>Search</button>
+            </div>
         </div>
     );
 };
