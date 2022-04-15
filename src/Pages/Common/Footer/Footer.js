3e144247195da8d7fc4bcedb4aadc7781e42bf8a
sabbirzzaman <sztonmoy7@gmail.com>
2022-04-15 21:49:19 +0600

    footer added

--- src/Pages/Common/Footer/Footer.js
+++ src/Pages/Common/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './Footer.css'
 
 const Footer = () => {
+    const date = new Date();
+    const year = date.getFullYear();
+
     return (
-        <div>
-            
+        <div className='footer-container'>
+            <p>Copyright &copy; {year} Red Onion</p>
         </div>
     );
 };
